9a8c0db9cbdee72396e692dc37550ca22ca6fb0e
pyszczekk <karolina.pieszczekk@gmail.com>
2018-11-17 15:23:42 +0100

    Lab6 - Drawer - Plots v1.0

--- Lab6/src/Drawer/DrawPlot.java
+++ Lab6/src/Drawer/DrawPlot.java
@@ -7,6 +7,10 @@ package Drawer;
 
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import javax.swing.JFrame;
@@ -19,23 +23,52 @@ import javax.swing.*;
 public class DrawPlot extends JFrame {
     
     public static void main(String [] argv){
+        try{
         DrawPlot d = new DrawPlot();
+        Plot plotPanel = new Plot();
         JPanel main = new JPanel();
-       
         JLabel l1 = new JLabel("<html>W(x) =a<sub>5</sub>x<sup>5</sup>+a<sub>4</sub>x<sup>4</sup>+a<sub>3</sub>x<sup>3</sup>+a<sub>2</sub>x<sup>2</sup>+a<sub>1</sub>x+a<sub>0</sub></html>", JLabel.CENTER);
         Font labelFont = l1.getFont();
         l1.setFont(new Font(labelFont.getName(), Font.PLAIN, 18));
         l1.setLocation(300,50);
-        JLabel a5=new JLabel("<html>a<sub>5</sub>=</html>",JLabel.LEFT);
-        JLabel a4=new JLabel("<html>a<sub>4</sub>=</html>",JLabel.LEFT);
-        JLabel a3=new JLabel("<html>a<sub>3</sub>=</html>",JLabel.LEFT);
-        JLabel a2=new JLabel("<html>a<sub>2</sub>=</html>",JLabel.LEFT);
-        JLabel a1=new JLabel("<html>a<sub>1</sub>=</html>",JLabel.LEFT);
-        JLabel a0=new JLabel("<html>a<sub>0</sub>=</html>",JLabel.LEFT);
-        JLabel labelList []={a5,a4,a3,a2,a1,a0};
+        JLabel al5=new JLabel("<html>a<sub>5</sub>=</html>",JLabel.LEFT);
+        JLabel al4=new JLabel("<html>a<sub>4</sub>=</html>",JLabel.LEFT);
+        JLabel al3=new JLabel("<html>a<sub>3</sub>=</html>",JLabel.LEFT);
+        JLabel al2=new JLabel("<html>a<sub>2</sub>=</html>",JLabel.LEFT);
+        JLabel al1=new JLabel("<html>a<sub>1</sub>=</html>",JLabel.LEFT);
+        JLabel al0=new JLabel("<html>a<sub>0</sub>=</html>",JLabel.LEFT);
+        JLabel labelList []={al5,al4,al3,al2,al1,al0};
         JPanel aPanel=new JPanel();
         JPanel aData = new JPanel();
-        
+        JPanel limPanel = new JPanel();
+        JLabel limlabel = new JLabel("enter the boundaries of x-axis",JLabel.LEFT);
+        limlabel.setLocation(0,0);
+        limPanel.add(limlabel);
+        JTextField lim1=new JTextField();
+        lim1.setPreferredSize(new Dimension(40,30));
+        JTextField lim2=new JTextField();
+        lim2.setPreferredSize(new Dimension(40,30));
+        lim1.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
+                char caracter = e.getKeyChar();
+                //System.out.println(caracter);
+                if (((caracter < '0') || (caracter > '9'))
+                        && (caracter != '-')) {
+                    e.consume();
+                }
+            }
+        });
+        lim2.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
+                char caracter = e.getKeyChar();
+                if (((caracter < '0') || (caracter > '9'))
+                        && (caracter != '-')) {
+                    e.consume();
+                }
+            }
+        });
+        limPanel.add(lim1);
+        limPanel.add(lim2);
         JTextField ad5=new JTextField();
         JTextField ad4=new JTextField();
         JTextField ad3=new JTextField();
@@ -48,27 +81,87 @@ public class DrawPlot extends JFrame {
             aPanel.add(labelList[i]);
             labelList[i].setPreferredSize(new Dimension(25,25));
             aFields[i].setPreferredSize( new Dimension( 50, 24 ) );
+            aFields[i].addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
+                char caracter = e.getKeyChar();
+                if (((caracter < '0') || (caracter > '9'))
+                        && (caracter != '-')) {
+                    e.consume();
+                }
+            }
+        });
+      
             aData.add(aFields[i]);
             
         }
-       
+        JPanel save = new JPanel();
+        JButton btn = new JButton("draw");
+        btn.setPreferredSize(new Dimension(200,30));
+        btn.addActionListener(new ActionListener()
+        {
+          public void actionPerformed(ActionEvent e)
+          {
+            int a[]=new int[6];
+             for(int i =0; i<aFields.length;i++){
+                
+                 String text = aFields[i].getText();
+                 a[i]=this.getNumber(text, a[i]);
+             }
+            int x1 = 0;
+            int x2 = 0;
+            x1 = this.getNumber(lim1.getText(), x1);
+            x2 = this.getNumber(lim2.getText(), x1);
+            plotPanel.setA(a[0],a[1],a[2],a[3],a[4],a[5]);
+            plotPanel.setLim(x1,x2);
+            
+          };
+
+            private int getNumber(String text, int x) {
+                boolean minus=false;
+                    if(text.charAt(0)=='-'){
+                                text=text.replace("-", "");
+                                minus=true;
+                            }
+                             x=Integer.parseInt(text);
+                             if(minus){
+                                 x*=-1;
+                             }
+                    return x;  
+            }
+             
+        });
+        save.add(btn);
+        save.setBounds(0,0,200,30);
+        save.setLocation(20,320);
         main.setBounds(0, 0, 800, 100);
         aPanel.setBounds(0,0,50,800);
         aPanel.setLocation(20,80);
         aData.setBounds(0,0,48,800);
         aData.setLocation(70,80);
         main.add(l1);
+        limPanel.setBounds(0,0,320,50);
+        limPanel.setLocation(20,260);
+        d.add(limPanel);
+        plotPanel.setBounds(0,0,800,400);
+        plotPanel.setLocation(0,400);
+        d.add(plotPanel);
         d.add(aData);
         d.add(aPanel);
         d.add(main);
+        d.add(save);
         d.setLayout(null);
-        d.setSize(800, 800);
+        d.setSize(600, 800);
         d.setVisible(true);
         d.addWindowListener(new WindowAdapter(){
             public void windowClosing(WindowEvent we){
                System.exit(0);
             }
         });
+        }
+        catch(UnsupportedOperationException e ){
+           System.out.println("you've got some errors ;p ");
+       }    
     }
+
     
 }
--- Lab6/src/Drawer/Plot.java
+++ Lab6/src/Drawer/Plot.java
@@ -10,6 +10,7 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.geom.QuadCurve2D;
 import static java.lang.Math.sqrt;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -20,51 +21,107 @@ import javax.swing.JPanel;
  * @author pyszczekk
  */
 public class Plot extends JPanel {
-    int data[][]=new int[300][2];
+    int data[][]=new int[1000][2];
     final int PAD = 10;
-  
+    int a5=0,a4=0,a3=0,a2=0,a1=0,a0=0;
+    int xStart, xEnd;
+    double xScale;
+    double yScale;
+   
+    int w=600;
+    int h=380;
+    int x0 = w/2;
+    int y0 = h/2;
+    
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D)g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
                             RenderingHints.VALUE_ANTIALIAS_ON);
-        int w = getWidth();
-        int h = getHeight();
+       
         g2.setColor(Color.WHITE);
         //g2.fillRect(PAD, PAD, w-2*PAD, h-2*PAD);
         g2.fillRect(0, 0, w, h);
         g2.setColor(Color.BLACK);
-        g2.drawLine(PAD, PAD-10, PAD, h);
+        g2.drawLine(w/2, PAD-10, w/2, h);
         
-        g2.drawLine(PAD, PAD-10, PAD-5, PAD);
-        g2.drawLine(PAD, PAD-10, PAD+5, PAD);
+        g2.drawLine(w/2, PAD-10, w/2-5, PAD);
+        g2.drawLine(w/2, PAD-10, w/2+5, PAD);
         
-        g2.drawLine(0, h-PAD, w, h-PAD);
-        g2.drawLine(w-PAD+10, h-PAD, w-PAD, h-PAD-5);
-        g2.drawLine(w-PAD+10, h-PAD, w-PAD, h-PAD+5);
-        double xScale = (w - 2*PAD)/10;
+        g2.drawLine(0, h/2, w, h/2);
+        g2.drawLine(w-PAD+10, h/2, w-PAD, h/2-5);
+        g2.drawLine(w-PAD+10, h/2, w-PAD, h/2+5);
+        xScale = (w - 2*PAD)/300;
         double maxValue = 100.0;
-        double yScale = (h - 2*PAD)/300;
+        yScale = (h - 2*PAD)/300;
         // The origin location.
         int x0 = PAD;
         int y0 = h-PAD;
         g2.setPaint(Color.red);
-        System.out.println(xScale+" "+yScale);
-        for(int j = 0; j < data.length; j++) {
-            
-            int x = x0 + (int)xScale*j;
-            int y = y0 - (int)yScale  *(j*j*j-j*j-5*j);
-            //int y = y0 - (int)yScale*(int)(sqrt(1-j*j));
-            System.out.println("x: "+x+" y: "+y);
-            data[j][0]=x;
-            data[j][1]=y;
-        }
-         for(int j = 1; j < data.length; j++) {
-             g2.drawLine(data[j-1][0],data[j-1][1], data[j][0],data[j][1]);
-         }
+        //System.out.println(xScale+" "+yScale);
+        if(a5!=0 || a4!=0 || a3!=0 || a2!=0 || a1!=0 || a0!=0)paintPlot(g2);
         //g2.scale(2.0,2.0);
     }
-  
-    
+    @Override
+    public void paint(Graphics g){
+        this.paintComponent(g);
+        //System.out.println("maluje sie");
+        repaint();
+    }
+    public void setA(int b5,int b4, int b3, int b2, int b1, int b0){
+        a5=b5;
+        a4=b4;
+        a3=b3;
+        a2=b2;
+        a1=b1;
+        a0=b0;
+    }
+    public void setLim(int x, int x1){
+        if(x<x1){
+            xStart=x;
+            xEnd=x1;
+        }else if(x>x1){
+            xStart=x1;
+            xEnd=x;
+        }else{
+            throw new UnsupportedOperationException("Wrong boundies");
+        }
+       
+    }
+    public void paintPlot(Graphics2D g2){
+        xScale = (780)/Math.abs(xEnd-xStart);
+        int max=(int)(function(xStart));
+        for(int j = xStart; j < xEnd+1; j++) {
+            
+            int x = j;
+            int y = (int)(function(j));
+            
+            data[j-xStart][0]=x;
+            data[j-xStart][1]=y;
+            
+            if(y>max)max=y;
+            
+        }
+        yScale=(h/2 - 20)/max;
+        if(yScale==0)yScale=1;
+        if(xScale==0)xScale=1;
+       // System.out.println(xScale+ " " + yScale + " max: "+max);
+         for(int j = 1; j < xEnd-xStart+1; j++) {
+             int _x=x0 + (int)xScale*data[j-1][0];
+             int _y=y0 -data[j-1][1]*(int)yScale;
+             int x_=x0 + (int)xScale*data[j][0];
+             int y_=y0 -data[j][1]*(int)yScale;
+             g2.drawLine(_x,_y, x_,y_);
+           //QuadCurve2D q = new QuadCurve2D.Float();
+            // draw QuadCurve2D.Float with set coordinates
+            //q.setCurve(_x, _y, (x_ -_x/2), (y_-_y/2), x_, y_);
+            //g2.draw(q);
+         }
+    }
+    public double function(int x){
+        return(a5*Math.pow(x,5)+a4*Math.pow(x,4)+a3*Math.pow(x,3)+a2*Math.pow(x,2)+a1*x+a0);
+        
+    }
+ 
 }
